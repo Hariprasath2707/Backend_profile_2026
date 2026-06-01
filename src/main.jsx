2250d027d1754ec586f7ba047ba12cbdf16eb684
Hariprasath2707 <hariprasath642@gmail.com>
2026-06-01 23:02:35 +0530

    fix: use HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'  // ← changed BrowserRouter to HashRouter
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Backend_profile_2026/">
+    <HashRouter>  {/* ← removed basename, HashRouter doesn't need it */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
