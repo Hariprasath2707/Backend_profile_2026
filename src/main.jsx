4e39ec2032eed1bd929863634c39ffca3e2373f3
Hariprasath2707 <hariprasath642@gmail.com>
2026-06-01 22:57:21 +0530

    fix: github pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Backend_profile_2026/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Backend_profile_2026/',
   plugins: [react()],
-  base: '/Backend_profile_2026/'
 })
